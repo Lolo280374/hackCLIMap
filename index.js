f43f400b2891c4497021455f5f2471d7b7c6a386
Lolo280374 <56828143+Lolo280374@users.noreply.github.com>
2026-02-17 21:28:14 +0100

    added mail listing :yay: (feature a)

--- index.js
+++ index.js
@@ -14,14 +14,14 @@ async function getAPIKey(){
     try {
         const data = await readFile(api_path, 'utf8');
         const config = JSON.parse(data);
-        console.log('saved key looks valid, continuing!');
-        console.log(`if your key is invalidated, please delete the file at '${api_path}'!`)
-        console.log('you can then restart the program and add your new key...')
+        console.log(chalk.green('saved key looks valid, continuing!'));
+        console.log(chalk.yellow(`if your key is invalidated, please delete the file at '${api_path}'!`))
+        console.log(chalk.yellow('you can then restart the program and add your new key...'))
         return config.api_key;
     } catch (error){
         if (error.code == 'ENOENT'){
-            console.log('no saved api key found, please enter your API key!');
-            console.log(`you can get an API key from 'https://mail.hackclub.com/my/api_keys'.`);
+            console.log(chalk.red('no saved api key found, please enter your API key!'));
+            console.log(chalk.yellow(`you can get an API key from 'https://mail.hackclub.com/my/api_keys'.`));
             const response = await prompt({
                 type: 'input',
                 name: 'resp_key',
@@ -31,7 +31,7 @@ async function getAPIKey(){
             const key = response.resp_key;
             await writeFile(api_path, JSON.stringify({ api_key: key }, null, 2));
             console.log('')
-            console.log(`saved your key under '${api_path}'!`);
+            console.log(chalk.yellow(`saved your key under '${api_path}'!`));
 
             return key;
         } else {
@@ -69,7 +69,7 @@ async function mail(){
     console.log('');
 
     if (resp_choice.toLowerCase() === 'list letters'){
-        list_letters()
+        await list_letters(api_key);
     };
 
     if (resp_choice.toLowerCase() === 'list packages'){
@@ -93,6 +93,54 @@ async function mail(){
     };
 }
 
+async function list_letters(api_key){
+    console.log(chalk.blue('loading!'));
 
+    try {
+        const response = await axios.get('https://mail.hackclub.com/api/public/v1/letters', {
+            headers: {
+                'Authorization': `Bearer ${api_key}`,
+                'Content-Type': 'application/json'
+            }
+        });
+
+        const letters = response.data.letters;
+        
+        if (letters.length === 0){
+            console.log(chalk.yellow('no letters found... maybe join a YSWS? :D'));
+            console.log(chalk.yellow('fun fact: get free welcome stickers in the toolbox!'))
+            return;
+        }
+
+        console.log(chalk.green(`showing a total of ${letters.length} letters:`));
+        console.log('');
+
+        letters.forEach(letter => {
+
+            let statusColor = chalk.white;
+            if (letter.status === 'received') statusColor = chalk.green;
+            if (letter.status === 'pending') statusColor = chalk.yellow;
+            if (letter.status === 'printed') statusColor = chalk.yellow;
+            if (letter.status === 'mailed') statusColor = chalk.cyan;
+
+            console.log(`${chalk.gray('id:')} ${letter.id}`);
+            console.log(`${chalk.gray('name:')} ${letter.title}`);
+            console.log(`${chalk.gray('status:')} ${statusColor(letter.status)}`);
+
+            if (letter.tags.length > 0){
+                console.log(`${chalk.gray('tags:')} ${letter.tags.join(', ')}`);
+            }
+
+            console.log('');
+            console.log(`learn more at '${chalk.underline(letter.public_url)}'!`);
+            console.log(chalk.dim(`-----------------------------------`));
+            console.log('');
+        });
+    } catch (error){
+        console.error('');
+        console.error(chalk.red(`an error occured while fetching letters...`));
+        console.error(chalk.red(`make sure the API key you're using is valid!`));
+    }
+}
 
 mail();
